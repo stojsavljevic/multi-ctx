e0f5d722c8b214e77e49b453ff17976375460dbb
Aleksandar Stojsavljevic <aleksandars@ccbill.com>
2017-01-18 12:24:21 +0100

    Minor improvements
    * switched to @SpringBootConfiguration instead of @Configuration

--- multi-ctx-app-servlets/src/main/java/com/alex/demo/ctx/child/second/ChildSecondCtxConfig.java
+++ multi-ctx-app-servlets/src/main/java/com/alex/demo/ctx/child/second/ChildSecondCtxConfig.java
@@ -1,15 +1,15 @@
 package com.alex.demo.ctx.child.second;
 
 
+import org.springframework.boot.SpringBootConfiguration;
 import org.springframework.context.annotation.Bean;
 import org.springframework.context.annotation.ComponentScan;
-import org.springframework.context.annotation.Configuration;
 import org.springframework.context.annotation.PropertySource;
 import org.springframework.web.servlet.config.annotation.EnableWebMvc;
 
 
 
-@Configuration
+@SpringBootConfiguration
 @ComponentScan
 @EnableWebMvc
 @PropertySource("classpath:context-second.properties")
