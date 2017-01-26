2af16e4e761050f292448c4a00feee2e980444c2
Aleksandar Stojsavljevic <aleksandars@ccbill.com>
2017-01-26 10:37:32 +0100

    Test implemented using random port

--- multi-ctx-app-embed/src/test/java/com/alex/demo/ctx/parent/MultiCtxWithParentProfileControllerTests.java
+++ multi-ctx-app-embed/src/test/java/com/alex/demo/ctx/parent/MultiCtxWithParentProfileControllerTests.java
@@ -9,9 +9,11 @@ import java.util.Map;
 import org.junit.Test;
 import org.junit.runner.RunWith;
 import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.boot.context.embedded.AnnotationConfigEmbeddedWebApplicationContext;
 import org.springframework.boot.test.context.SpringBootTest;
 import org.springframework.boot.test.context.SpringBootTest.WebEnvironment;
 import org.springframework.boot.test.web.client.TestRestTemplate;
+import org.springframework.context.ApplicationContext;
 import org.springframework.test.annotation.DirtiesContext;
 import org.springframework.test.annotation.DirtiesContext.ClassMode;
 import org.springframework.test.context.ActiveProfiles;
@@ -21,19 +23,22 @@ import org.springframework.test.context.junit4.SpringRunner;
 
 @ActiveProfiles("parent")
 @RunWith(SpringRunner.class)
-@SpringBootTest(webEnvironment = WebEnvironment.DEFINED_PORT)
+@SpringBootTest(webEnvironment = WebEnvironment.RANDOM_PORT)
 @DirtiesContext(classMode = ClassMode.AFTER_CLASS)
 public class MultiCtxWithParentProfileControllerTests {
 
     @Autowired
     TestRestTemplate restTemplate;
+    
+    @Autowired
+	ApplicationContext applicationContext;
 
     @SuppressWarnings("unchecked")
     @Test
     public void testChildWithParent()
             throws Exception {
-
-        Map<String, String> response = restTemplate.getForObject("http://localhost:8082/child", Map.class);
+    	
+        Map<String, String> response = restTemplate.getForObject("http://localhost:" + getChildPort() + "/child", Map.class);
 
         assertEquals("parent_bean", response.get("parentBean"));
         assertEquals("child_bean", response.get("childBean"));
@@ -47,11 +52,17 @@ public class MultiCtxWithParentProfileControllerTests {
     public void testParent()
             throws Exception {
 
-        Map<String, String> response = restTemplate.getForObject("http://localhost:8080/parent", Map.class);
+        Map<String, String> response = restTemplate.getForObject("/parent", Map.class);
 
         assertEquals("parent_bean", response.get("parentBean"));
         assertNull(response.get("childBean"));
         assertEquals("common_prop", response.get("parentProperty"));
         assertEquals("null", response.get("childProperty"));
+    }
+    
+    int getChildPort(){
+    	
+    	AnnotationConfigEmbeddedWebApplicationContext childContext = applicationContext.getBean(AnnotationConfigEmbeddedWebApplicationContext.class);
+    	return childContext.getEmbeddedServletContainer().getPort();
     }
 }
