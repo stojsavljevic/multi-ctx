79c11195d3f55ea89216423393ff374fb9e088f9
Aleksandar Stojsavljevic <aleksandars@ccbill.com>
2022-12-22 14:22:51 +0100

    Updated to 3.0.0

--- multi-ctx-app-embed/src/main/java/com/alex/demo/ctx/child/ChildCtxConfig.java
+++ multi-ctx-app-embed/src/main/java/com/alex/demo/ctx/child/ChildCtxConfig.java
@@ -5,8 +5,8 @@ import java.util.List;
 import java.util.Objects;
 import java.util.Optional;
 
-import javax.servlet.http.HttpServletRequest;
-import javax.servlet.http.HttpServletResponse;
+import jakarta.servlet.http.HttpServletRequest;
+import jakarta.servlet.http.HttpServletResponse;
 
 import org.springframework.beans.factory.BeanFactoryUtils;
 import org.springframework.beans.factory.ListableBeanFactory;
